695e829c00d1c28815d59c90db6b4cc91da737fe
EvgeniyHalimon <halimonevgeniy@gmail.com>
2023-09-21 21:34:51 +0300

    updated store functionality

--- src/hooks/useActions.ts
+++ src/hooks/useActions.ts
@@ -1,13 +1,13 @@
 import { bindActionCreators } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
 
-import { setMatches } from '../shared/store/matchmakingSlice';
+import { setMatches, setPlayers, setMatchResults, setTournamentResults } from '../shared/store/matchmakingSlice';
 
 
 const useActions = () => {
   const dispatch = useDispatch();
 
-  return bindActionCreators({ setMatches }, dispatch);
+  return bindActionCreators({ setMatches, setPlayers, setMatchResults, setTournamentResults }, dispatch);
 };
 
 export default useActions;
--- src/shared/store/matchmakingSlice.ts
+++ src/shared/store/matchmakingSlice.ts
@@ -1,13 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+import { ITournamentResults } from '../types';
+
 import type { PayloadAction } from '@reduxjs/toolkit';
 
-interface CounterState {
-  matches: string[][]
+interface IMatchMakingSliceState {
+  matches: string[][],
+  players: string[],
+  matchResults: string[][],
+  tournamentResults: [] | ITournamentResults[]
 }
 
-const initialState: CounterState = {
+const initialState: IMatchMakingSliceState = {
   matches: [],
+  players: [],
+  matchResults: [],
+  tournamentResults: [],
 };
 
 export const matchmakingSlice = createSlice({
@@ -17,7 +25,16 @@ export const matchmakingSlice = createSlice({
     setMatches: (state, action: PayloadAction<string[][]>) => {
       state.matches = action.payload;
     },
+    setPlayers: (state, action: PayloadAction<string[]>) => {
+      state.players = action.payload;
+    },
+    setMatchResults: (state, action: PayloadAction<string[][]>) => {
+      state.matchResults = action.payload;
+    },
+    setTournamentResults: (state, action: PayloadAction<ITournamentResults[]>) => {
+      state.tournamentResults = action.payload;
+    },
   },
 });
 
-export const { setMatches } = matchmakingSlice.actions;
+export const { setMatches, setPlayers, setMatchResults, setTournamentResults } = matchmakingSlice.actions;
